8c8cabf736121cfd3a99a63646cfe7d03da2430e
TaurusA <realtaurus97@gmail.com>
2024-01-12 10:12:08 -0600

    Taurus Variables Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string newCar = "Jeep Trackhawk";
+            int carDoor = 4;
+            char carRatings = 'A';
+            bool isSuv = true;
+            double topSpeed = 2.8;
+            decimal carPrice = 100000.58m;
+
+            Console.WriteLine($"My dream car is the {newCar}, it is a {carDoor} door {isSuv}.");
+            Console.WriteLine($"It has a top speed of {topSpeed} mpr");
+            Console.WriteLine($"It is far from cheap and has an msrp of {carPrice}.");
+            Console.WriteLine($"It has and excelent rating of {carRatings} on Autoreviews");
         }
     }
 }
